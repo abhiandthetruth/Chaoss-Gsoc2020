133c540b00a8d3e7323e6bb550882a43012354f3
abhiandthetruth <abhiandthetruth@gmail.com>
2020-03-16 01:18:17 +0530

    Modified Microtask-2

--- Microtask-2/percevalGithubAndGitLab.py
+++ Microtask-2/percevalGithubAndGitLab.py
@@ -1,51 +1,57 @@
 import perceval.backends.core.github as github
 import perceval.backends.core.gitlab as gitlab
+
 from configparser import ConfigParser
 
+
 """Getting the configuration"""
+
 config = ConfigParser()
 config.read('config.ini')
 
+
 """Github Section"""
 
-# Specifying Owner of Repositories
-owner = 'abhiandthetruth'
-
-# Specifying name of the repository
-repository = 'perceval-voice'
+# Getting the repos
+repos = config.get('Github', 'repos').split(',')
 
 # Getting the key
-api_keys= [config.get('ApiKeys', 'github_key')]
+api_keys = [config.get('Github', 'key')]
 
-# Creating the object
-repo = github.GitHub(owner=owner, repository=repository, api_token=api_keys)
+if '' not in repos:
+    for repo in repos:
+        owner = repo.split('/')[0].strip()
+        repository = repo.split('/')[1].strip()
+        print(owner, repository)
+        # Creating the object
+        repo = github.GitHub(owner=owner, repository=repository, api_token=api_keys)
+        # Getting list of issues and prs
+        issueList = repo.fetch(category='issue')
+        prList = repo.fetch(category='pull_request')
+        # printing the issues
+        for issue in issueList:
+            print(issue)
 
-# Getting list of issues and prs
-issueList = repo.fetch(category='issue')
-prList = repo.fetch(category='pull_request')
 
-# printing the issues
-for issue in issueList:
-    print(issue)
+"""Gitlab Section"""
 
-"""GitLab Section"""
-
-# Specifying Owner of Repositories
-owner = 'pgjones'
-
-# Specifying name of the repository
-repository = 'quart'
+# Getting the repos
+repos = config.get('Gitlab', 'repos').split(',')
 
 # Getting the key
-api_keys = config.get('ApiKeys', 'gitlab_key')
+api_keys = config.get('Gitlab', 'key')
 
-# Creating the object of the repository
-repo = gitlab.GitLab(owner=owner, repository=repository, api_token=api_keys)
+if '' not in repos:
+    for repo in repos:
+        owner = repo.split('/')[0].strip()
+        repository = repo.split('/')[1].strip()
+        print(owner, repository)
+        # Creating the object of the repository
+        repo = gitlab.GitLab(owner=owner, repository=repository, api_token=api_keys)
+        # Getting list of issues and prs
+        issueList = repo.fetch(category='issue')
+        prList = repo.fetch(category='pull_request')
+        # printing the issues
+        for issue in issueList:
+            print(issue)
 
-# Getting list of issues and prs
-issueList = repo.fetch(category='issue')
-prList = repo.fetch(category='pull_request')
-
-# printing the issues
-for issue in issueList:
-    print(issue)
