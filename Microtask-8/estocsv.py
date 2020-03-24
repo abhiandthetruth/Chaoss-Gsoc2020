bc4c81d4de49fcb4c7db05fd5df2f67df4106030
abhiandthetruth <abhiandthetruth@gmail.com>
2020-03-25 01:47:48 +0530

    refractored estocsv.py

--- Microtask-8/estocsv.py
+++ Microtask-8/estocsv.py
@@ -1,34 +1,53 @@
 import requests
+import argparse, sys
 import json,csv
 from configparser import ConfigParser
 
-# Get the parser ready 
-config = ConfigParser()
-config.read('/home/abhia/Documents/config.ini')
-
-# Getting the Global Parameters
-ELASTIC_SEARCH_URL = config.get('elastic_search', 'url')
-INDEX = config.get('elastic_search', 'index')
-FIELDS = [string.strip() for string in config.get('elastic_search', 'fields').split(',')]
+# Global variables
 SESSION = requests.Session()
-PAYLOAD = {
+
+def get_args():
+    """
+    Get params from cli required for exexution of module
+    :argument --cfg: Path to config.ini
+    """
+
+    parser = argparse.ArgumentParser(description="Get the path to cfg")
+    parser.add_argument('--cfg', dest='cfg_path', 
+                        help="Path to config.ini")
+    if len(sys.argv) == 0:
+        parser.print_help()
+        sys.exit()
+    args = parser.parse_args()
+    return args
+
+def get_hits(es_url, index, fields, size=100):
+    """
+    Get all hits from elastic search for a given index
+    
+    This function returns all hits i.e., records for a given 'index' containing only the
+    'fields' specified where the total records in each response equals 'size'. 
+    
+    :param es_url: url to connect elastic search
+    :param index: the index from which records are to be retrieved
+    :param fields: the list of fields that need to fetched from the index
+    :param size: the size of response to fetch
+    """
+
+    url = es_url + "/" + index + "/_search?scroll=1m&size="+str(size)
+    payload = {
             "_source": {
-                "includes": FIELDS
+                "includes": fields
             },
             "query" : {
                 "match_all" : {}
             }
         }
-
-
-def get_hits(size=100):
-    """Returns all hits given index, fields, url globally and passed size of one fetch"""
-    url = ELASTIC_SEARCH_URL + "/" + INDEX + "/_search?scroll=1m&size="+str(size)
-    r = SESSION.post(url, data=json.dumps(PAYLOAD), headers={'Content-Type':'application/json'})
+    r = SESSION.post(url, data=json.dumps(payload), headers={'Content-Type':'application/json'})
     raw_data = r.json()
     total_hits = raw_data['hits']['hits']
     scroll_id = raw_data['_scroll_id']
-    scroll_url = ELASTIC_SEARCH_URL + "/_search/scroll"
+    scroll_url = es_url + "/_search/scroll"
     scroll_payload = {
         "scroll" : "1m", 
         "scroll_id" : scroll_id
@@ -44,7 +63,17 @@ def get_hits(size=100):
     return total_hits
 
 def generate_csv(hits, filename):
-    """Generates the csv given the hits with filename as the name"""
+    """
+    Generates the csv file from a set of JSON documents
+    
+    This function receives as input a list of JSON 
+    documents, and stores them to a CSV file, which name 
+    is defined by the attribute `filename`.
+
+    :param hits: a list of JSON documents
+    :param filename: the output file where to store the CSV data
+    """
+
     headers = [key for key in hits[0]['_source']]
     records = [headers]
     for hit in hits:
@@ -55,12 +84,23 @@ def generate_csv(hits, filename):
         writer = csv.writer(file)
         writer.writerows(records)
 
-def main():
-    """The main function"""
-    hits = get_hits()
-    generate_csv(hits, INDEX)
+def main(config):
+    """
+    Get the params from config and execute the module
+    
+    :param config: the parser for the configuration file
+    """ 
+
+    es_url = config.get('elastic_search', 'url')
+    index = config.get('elastic_search', 'index')
+    fields = [string.strip() for string in config.get('elastic_search', 'fields').split(',')]
+    hits = get_hits(es_url, index, fields)
+    generate_csv(hits, index)
     print("Done!!")
 
 if __name__ == "__main__":
-    main()
+    args = get_args()
+    config = ConfigParser()
+    config.read(args.cfg_path)
+    main(config)
 
